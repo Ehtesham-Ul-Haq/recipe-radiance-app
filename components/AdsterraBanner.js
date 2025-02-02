02a4b624c9174d05b24a6516af95b9cf2bfc4478
Ehtesham Ul Haq <ihtishamb7@gmail.com>
2025-02-03 02:41:58 +0500

    updated

--- components/AdsterraBanner.js
+++ components/AdsterraBanner.js
@@ -1,18 +1,27 @@
 import Script from "next/script";
+import { useEffect } from "react";
 
 const AdsterraBanner = () => {
+  useEffect(() => {
+    window.atOptions = {
+      key: "865a5f6a6551822787590b50f0039e2c",
+      format: "iframe",
+      height: 90,
+      width: 728,
+      params: {},
+    };
+  }, []);
+
   return (
     <div>
-      {/* Adsterra Script */}
+      {/* Adsterra Ad Script */}
       <Script
         strategy="afterInteractive"
-        async
-        data-cfasync="false"
-        src="//pl25751472.profitablecpmrate.com/9a38ae1832e7604b7eb41854d8c92c8e/invoke.js"
+        src="//www.highperformanceformat.com/865a5f6a6551822787590b50f0039e2c/invoke.js"
       />
       
       {/* Ad Container */}
-      <div id="container-9a38ae1832e7604b7eb41854d8c92c8e"></div>
+      <div id="container-865a5f6a6551822787590b50f0039e2c"></div>
     </div>
   );
 };
